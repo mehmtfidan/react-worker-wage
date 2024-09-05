4a94d2ed90a419149bdfdebb2cc41b60d460d55a
Mehmet Fidan <mehmetfidan2090@gmail.com>
2024-09-05 17:19:27 +0300

    added workerList component & errorModal component

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -3,6 +3,7 @@ const Button = (props) => {
     <button
       className={`p-2 text-lg bg-teal-700 text-white  ${props.className}`}
       type={props.type || "button"}
+      onClick={props.onClick}
     >
       {props.children}
     </button>
--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,57 +1,80 @@
 import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddWorker = () => {
+const AddWorker = (props) => {
   const [enteredWorkerName, setEnteredWorkerName] = useState("");
   const [enteredWage, setEnteredWage] = useState("");
+  const [error, setError] = useState();
   const minimumWage = 5000;
   const addWorkerHandler = (e) => {
     e.preventDefault();
-    if (
-      enteredWorkerName.trim().lengthlength === 0 ||
-      enteredWage.trim().length === 0
-    ) {
+    if (enteredWorkerName.trim().length === 0) {
+      setError({
+        title: "İsim Alanı Zorunludur!",
+        message: "Lütfen bir isim giriniz.",
+      });
       return;
     }
-    if(+enteredWage < minimumWage) {
-        return;
+
+    if (+enteredWage < minimumWage) {
+      setError({
+        title: "Maaş Alanı Zorunludur!",
+        message: `Lütfen ${minimumWage} değerinden büyük bir maaş değeri giriniz.`,
+      });
+      return;
     }
+
+    props.setWorkers((prevState) => [
+      {
+        id: Math.floor(Math.random() * 1000),
+        name: enteredWorkerName,
+        wage: enteredWage,
+      },
+      ...prevState,
+    ]);
     setEnteredWorkerName("");
     setEnteredWage("");
-    console.log(enteredWorkerName, enteredWage);
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card addClass="mt-10">
-      <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
-        <label htmlFor="name" className="font-medium">
-          Çalışan İsmi
-        </label>
-        <input
-          type="text"
-          className="max-w-[40rem] w-full mx-auto border p-2"
-          placeholder="Çalışan ismi yazınız"
-          id="name"
-          onChange={(e) => setEnteredWorkerName(e.target.value)}
-          value={enteredWorkerName}
-        />
-        <label htmlFor="wage" className="font-medium">
-          Maaş Miktarı
-        </label>
-        <input
-          type="number"
-          className="max-w-[40rem] w-full mx-auto border p-2"
-          placeholder="Maaş miktarı yazınız"
-          id="wage"
-          onChange={(e) => setEnteredWage(e.target.value)}
-          value={enteredWage}
-        />
-        <Button className="mt-2" type="submit">
-          Ekle
-        </Button>
-      </form>
-    </Card>
+    <div>
+      {error && <ErrorModal onConfirm={errorHandler} error={error} />}
+      <Card addClass="mt-10">
+        <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
+          <label htmlFor="name" className="font-medium">
+            Çalışan İsmi
+          </label>
+          <input
+            type="text"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Çalışan ismi yazınız"
+            id="name"
+            onChange={(e) => setEnteredWorkerName(e.target.value)}
+            value={enteredWorkerName}
+          />
+          <label htmlFor="wage" className="font-medium">
+            Maaş Miktarı
+          </label>
+          <input
+            type="number"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Maaş miktarı yazınız"
+            id="wage"
+            onChange={(e) => setEnteredWage(e.target.value)}
+            value={enteredWage}
+          />
+          <Button className="mt-2" type="submit">
+            Ekle
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
